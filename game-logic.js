d7774cdd0e10003747486c41024101c8477833b9
Moeez Ali <moeez@marketlytics.com>
2014-05-17 12:54:29 +0500

    initial

--- game-logic.js
+++ game-logic.js
@@ -31,29 +31,37 @@ var game = {
 	level :0 ,
 	isthere : [],
 
-	numberSet : function(levelRange) {							/*(Math.random() * (max - min + 1)) + min;*/
-		game.level+=1;
+	numberSet : function(levelRange,set1,set2,minimum) {							/*(Math.random() * (max - min + 1)) + min;*/
+		
+		
+		console.log(this.level);
+		console.log(levelRange);
 
-		panOne = parseInt(Math.random() * ((levelRange+15)-10+1) + 10, 10);
-		panTwo = panOne-2;
-								
-		p1=this.generateSequence(panOne, 4, 2);
-		p2=this.generateSequence(panTwo, 3, 2);
+		if(levelRange<=5) {
+			panOne = parseInt(Math.random() * ((levelRange+15)-10+1) + 10, 10);
+			panTwo = panOne-2;
+		}
+		else if (levelRange<=16) {
+			panOne = parseInt(Math.random() * ((levelRange+70)-30+1) + 30, 10);
+			panTwo = panOne-2;
+		}
+
+		p1=this.generateSequence(panOne,set1,minimum);
+		p2=this.generateSequence(panTwo,set2,minimum);
 
 		var sequence = p1.concat(p2);
         $('#numberSet').html(sequence.join(','));
 							
 		this.result = (panOne+panTwo)/2;
-		// console.log(this.result);
+		console.log(this.result);
 
+		game.isthere.push(this.result);
+		
 		if(game.isthere.indexOf(this.result) !== -1) {
 			game.isthere.splice(game.level-1,1);
 			game.level--;
 			return;
 		}
-
-		game.isthere.push(this.result);
-		// console.log(game.isthere);s
 	},
 
 	gameFunction : function() {
@@ -61,16 +69,20 @@ var game = {
 		// balanceType : where the number was entered (left / right)
 		// TODO:
 		// everytime a user enters a number in any weight
-		
+
+		game.level+=1;
 		game.result = 0;
+
+		$('#result').html("");
+		$('#level').html("LEVEL : "+game.level);
 
 		if(this.level<=33){
 
-			if(this.level<=10){
-				game.numberSet(10);
+			if(this.level<=5){
+				game.numberSet(5,4,3,2);
 			}
 			else if(this.level<=16){
-				game.numberSet(16);
+				game.numberSet(6,5,5,2);
 			}
 			else if(this.level<=24){
 				game.numberSet(24);
@@ -117,6 +129,8 @@ var game = {
 		}
 
 		if(sum1==this.result && sum2==this.result) {
+			$('#box1').val('');
+			$('#box2').val('');
 			$('#result').html("YOU WIN!!");
 			this.gameFunction();
 		}
